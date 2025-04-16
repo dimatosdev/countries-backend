67839ab39411c27cf65c59577b376cbe24285a54
dimatosdev <dimatosdev@gmail.com>
2025-04-16 08:44:21 -0300

    Create DTO

---
+++ src/main/java/com/mngs/countries/countries_backend/dto/UsuarioAutenticado.java
@@ -0,0 +1,9 @@
+package com.mngs.countries.countries_backend.dto;
+
+public class UsuarioAutenticado {
+    private String login;
+    private String nome;
+    private String token;
+    private boolean administrador;
+    private boolean autenticado;
+}
