8a3a3c14f3f436ffc6fde3d58d7b9d50bc287d06
dimatosdev <dimatosdev@gmail.com>
2025-04-15 22:39:18 -0300

    First Commit

---
+++ src/test/java/com/mngs/countries/countries_backend/CountriesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mngs.countries.countries_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CountriesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
