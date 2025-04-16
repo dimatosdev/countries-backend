c2a93e71e86797b5fa06396e6b12f4670ff168e9
dimatosdev <dimatosdev@gmail.com>
2025-04-16 08:31:57 -0300

    Seed H2

--- src/main/java/com/mngs/countries/countries_backend/entities/Pais.java
+++ src/main/java/com/mngs/countries/countries_backend/entities/Pais.java
@@ -16,4 +16,43 @@ public class Pais {
 
     @Column(unique=true, nullable=false)
     private String gentilico;
+
+    public Pais(int id, String nome, String sigla, String gentilico) {
+        this.id = id;
+        this.nome = nome;
+        this.sigla = sigla;
+        this.gentilico = gentilico;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public String getGentilico() {
+        return gentilico;
+    }
+
+    public void setGentilico(String gentilico) {
+        this.gentilico = gentilico;
+    }
 }
--- src/main/java/com/mngs/countries/countries_backend/entities/Usuario.java
+++ src/main/java/com/mngs/countries/countries_backend/entities/Usuario.java
@@ -3,6 +3,7 @@ package com.mngs.countries.countries_backend.entities;
 import jakarta.persistence.*;
 
 @Entity
+
 public class Usuario {
 
     @Id
@@ -17,4 +18,54 @@ public class Usuario {
 
     private String nome;
     private boolean administrador;
+
+    public Usuario(Long id, String login, String senha, String nome, boolean administrador) {
+        this.id = id;
+        this.login = login;
+        this.senha = senha;
+        this.nome = nome;
+        this.administrador = administrador;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public boolean isAdministrador() {
+        return administrador;
+    }
+
+    public void setAdministrador(boolean administrador) {
+        this.administrador = administrador;
+    }
+
+
 }
